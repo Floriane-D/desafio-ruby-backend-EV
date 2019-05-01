a0df4c2402e54a6f5300acbd014dac81224dd737
Floriane Didier <florianedidier@gmail.com>
2019-05-01 12:37:25 -0300

    Adding some validations to the models

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,4 +1,22 @@
 class Competition < ApplicationRecord
   has_many :results, dependent: :destroy
   has_many :athletes, -> { distinct }, through: :results
+
+  validates :name,
+    presence: true,
+    uniqueness: { case_sensitive: false }
+
+  validates :unit,
+    presence: true
+
+  validates :criteria_to_win,
+    presence: true,
+    inclusion: { in: %w(min max),
+    message: "%{value} has to be either min or max" }
+
+  validates :max_number_of_attempts,
+    presence: true,
+    numericality: { only_integer: true, greater_than: 0 }
+
+
 end
--- app/models/result.rb
+++ app/models/result.rb
@@ -1,4 +1,34 @@
 class Result < ApplicationRecord
   belongs_to :athlete
   belongs_to :competition
+
+  validates :value,
+    presence: true,
+    numericality: { greater_than: 0 }
+
+  validate :competition_is_still_running
+  validate :athlete_still_has_attempts
+
+  scope :from_athlete, -> (athlete) { where(athlete: athlete) }
+
+  protected
+
+  def competition_is_still_running
+    if competition.try(:finished?)
+      error_message = "You cannot add a new result because the competition is finished"
+      self.errors.add(:competition, error_message)
+    end
+  end
+
+  def athlete_still_has_attempts
+    if competition && athlete
+      number_of_attempts = competition.results.from_athlete(athlete).count
+      max_attempts = competition.max_number_of_attempts
+
+      if number_of_attempts >= max_attempts
+        error_message = "The athlete already reached the maximum number of attempts for this competition"
+        self.errors.add(:athlete, error_message)
+      end
+    end
+  end
 end
