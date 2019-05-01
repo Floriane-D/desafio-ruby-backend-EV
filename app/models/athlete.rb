5d50be07f492de9bb1d8c3f46999c50134c048b8
Floriane Didier <florianedidier@gmail.com>
2019-05-01 11:42:54 -0300

    Adding "has_many" associations to Athletes and Competitions

--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -1,2 +1,4 @@
 class Athlete < ApplicationRecord
+  has_many :results, dependent: :destroy
+  has_many :competitions, -> { distinct }, through: :results
 end
--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,2 +1,4 @@
 class Competition < ApplicationRecord
+  has_many :results, dependent: :destroy
+  has_many :athletes, -> { distinct }, through: :results
 end
