0d0869b6c1a9513e50cf5a608188b4be870da402
Floriane Didier <florianedidier@gmail.com>
2019-05-02 19:15:02 -0300

    Completing Database seeds

--- app/views/competitions/show.json.jbuilder
+++ app/views/competitions/show.json.jbuilder
@@ -1,5 +1,3 @@
 json.extract! @competition, :id, :name, :unit, :finished, :criteria_to_win, :max_number_of_attempts
 
-json.ranking @competition.ranking do |result|
-  json.extract! result, :id, :athlete, :value
-end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+require 'faker'
+
+puts 'Creating 2 competitions...'
+
 one_hundred_metres_race = Competition.create(
   name: "100m classificatoria 1",
   unit: "s",
@@ -7,3 +11,35 @@ javelin_throw = Competition.create(
   name: "Dardo semifinal",
   unit: "m",
   max_number_of_attempts: 3)
+
+puts 'Creating 5 athletes...'
+5.times do
+  athlete = Athlete.new(name: Faker::Name.name)
+  athlete.save!
+end
+
+puts 'Creating 5 results for the 100m race...'
+athletes_for_race = (1..5).to_a
+5.times do
+  result = Result.new(
+    competition_id: 1,
+    athlete_id: athletes_for_race.sample,
+    value: Faker::Number.between(7, 15)
+  )
+  result.save!
+  athletes_for_race.delete(result.athlete_id)
+end
+
+puts 'Creating 15 results for the Javelin throw (3 attempts per athlete)...'
+athletes_for_javelin = (1..5).to_a * 3
+15.times do
+  result = Result.new(
+    competition_id: 2,
+    athlete_id: athletes_for_javelin.sample,
+    value: Faker::Number.between(70, 100)
+  )
+  result.save!
+  athletes_for_javelin.slice!(athletes_for_javelin.index(result.athlete_id))
+end
+
+puts 'Finished!'
